fdabcf53191386753921623b579c76d3378d268f
Isaac Yanez <isaac231467@gmail.com>
2023-06-22 11:30:23 -0500

    removed some code I did not need anymore

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -17,6 +17,25 @@ export default async function page() {
   return (
     <div>
       <BlogList posts={posts} />
+
+      {/* TODO:  Make this into a component */}
+      {/* <hr className="border-[#F7AB0A] my-10" />
+      <div className="flex flex-col lg:flex-row lg:space-x-5 justify-between font-bold px-10 py-5 mb-5">
+        <div>
+          <h1 className="text-7xl">Who we are</h1>
+          <h2 className="mt-5 md:mt-0">
+            <p className="max-w-lg">
+              We're a community of{" "}
+              <span className="underline decoration-4 decoration-[#F7AB0A] text-clip">
+                passionate Steelers fans who live and breathe the team.
+              </span>{" "}
+              From the Terrible Towel to game day excitement, there's no better
+              place to be than with us. Come experience Steelers football like
+              never before!
+            </p>
+          </h2>
+        </div>
+      </div> */}
     </div>
   );
 }
